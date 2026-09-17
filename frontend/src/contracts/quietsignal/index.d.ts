f8851e2f7267286e4bdc94fdaacf51d0dd1d616e
Mondrita-Dutta <mondritadebayan123@gmail.com>
2026-09-17 14:14:09 +0530

    fix: Resolve CI pipeline dependency issues

---
+++ frontend/src/contracts/quietsignal/index.d.ts
@@ -0,0 +1,57 @@
+import type * as __compactRuntime from '@midnight-ntwrk/compact-runtime';
+
+export type SignalToken = { topicId: Uint8Array; secret: Uint8Array };
+
+export type Witnesses<PS> = {
+  secretEligibilityHash(context: __compactRuntime.WitnessContext<Ledger, PS>): [PS, Uint8Array];
+}
+
+export type ImpureCircuits<PS> = {
+  broadcastSignal(context: __compactRuntime.CircuitContext<PS>,
+                  topicId_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+}
+
+export type ProvableCircuits<PS> = {
+  broadcastSignal(context: __compactRuntime.CircuitContext<PS>,
+                  topicId_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+}
+
+export type PureCircuits = {
+}
+
+export type Circuits<PS> = {
+  broadcastSignal(context: __compactRuntime.CircuitContext<PS>,
+                  topicId_0: Uint8Array): __compactRuntime.CircuitResults<PS, []>;
+}
+
+export type Ledger = {
+  topics: {
+    isEmpty(): boolean;
+    size(): bigint;
+    member(key_0: Uint8Array): boolean;
+    lookup(key_0: Uint8Array): bigint;
+    [Symbol.iterator](): Iterator<[Uint8Array, bigint]>
+  };
+  signalTokens: {
+    isEmpty(): boolean;
+    size(): bigint;
+    member(elem_0: Uint8Array): boolean;
+    [Symbol.iterator](): Iterator<Uint8Array>
+  };
+}
+
+export type ContractReferenceLocations = any;
+
+export declare const contractReferenceLocations : ContractReferenceLocations;
+
+export declare class Contract<PS = any, W extends Witnesses<PS> = Witnesses<PS>> {
+  witnesses: W;
+  circuits: Circuits<PS>;
+  impureCircuits: ImpureCircuits<PS>;
+  provableCircuits: ProvableCircuits<PS>;
+  constructor(witnesses: W);
+  initialState(context: __compactRuntime.ConstructorContext<PS>): __compactRuntime.ConstructorResult<PS>;
+}
+
+export declare function ledger(state: __compactRuntime.StateValue | __compactRuntime.ChargedState): Ledger;
+export declare const pureCircuits: PureCircuits;
